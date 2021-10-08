eefb61fc158d7819fee27e6926ba1427c6bab017
NikCapko <nik.capko@mail.ru>
2021-10-08 12:32:42 +0300

    Исправление восстановления состояния экрана списка слов

--- app/src/main/java/com/nik/capko/memo/ui/words/list/WordListView.kt
+++ app/src/main/java/com/nik/capko/memo/ui/words/list/WordListView.kt
@@ -4,11 +4,14 @@ import com.nik.capko.memo.base.view.ProgressMvpView
 import com.nik.capko.memo.data.Word
 import moxy.MvpView
 import moxy.viewstate.strategy.AddToEndSingleStrategy
+import moxy.viewstate.strategy.OneExecutionStateStrategy
 import moxy.viewstate.strategy.StateStrategyType
 
 @StateStrategyType(AddToEndSingleStrategy::class)
 interface WordListView : MvpView, ProgressMvpView {
     fun showWords(wordsList: List<Word>)
     fun showClearDatabaseDialog()
+
+    @StateStrategyType(OneExecutionStateStrategy::class)
     fun speakOut(word: String?)
 }
